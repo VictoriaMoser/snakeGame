c217bb41659a9c03bd58015ac32ca8028e669930
Victoria Moser <moservictoria02@gmail.com>
2016-12-13 22:10:27 -0500

    trying to fix snake eating the food

--- snake.js
+++ snake.js
@@ -5,6 +5,8 @@ function Snake() {
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
 
   this.dir = function(x,y) {
     this.xspeed = x;
@@ -13,13 +15,24 @@ function Snake() {
 
   this.eat = function(postion) {
     var distance = dist(this.x, this.y, postion.x, postion.y);
+    // if it is the food the tall should increase by one
     if (distance < 1) {
+      total++;
       return true;
     } else {
       return false;
     }
   }
   this.update = function() {
+    if (this.total === this.tail.length) {
+    // as it moves and eats something, shifts all the spots and add it to the end of the array or beginning of the array?
+      for (var i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+      // we want the last spot to = createVector
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
     this.x = this.x + this.xspeed*scl;
     this.y = this.y + this.yspeed*scl;
 
@@ -28,8 +41,12 @@ function Snake() {
     this.y = constrain(this.y, 0, height - scl);
   }
 
+
   this.show = function() {
     fill(255);
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
     rect(this.x, this.y, scl, scl);
   }
 }
